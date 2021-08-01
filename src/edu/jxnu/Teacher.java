02062a3433b99e0865938e8495dbaac5bac58c92
wsx <wushuixiu@jxnu.edu.cn>
2021-08-01 19:51:43 +0800

    我新增了Teacher的类

---
+++ src/edu/jxnu/Teacher.java
@@ -0,0 +1,45 @@
+package edu.jxnu;
+
+/**
+ * description:描述
+ * author:江西师大
+ * time:19:50
+ */
+public class Teacher {
+    private String name;
+    private int age;
+    private int sex;
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
